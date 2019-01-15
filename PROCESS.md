a30932eeec4945d0a24247168d9d33cd0c6ea2ed
Jerinca <jerinca@live.nl>
2019-01-15 20:31:21 +0100

    update process book & code

--- PROCESS.md
+++ PROCESS.md
@@ -28,3 +28,12 @@ I can use the users input to search for the right API.
 Furthermore the interactive graph with historical stock information is not going to be a problem. The idea now is to create a barchart underneath that is going to show the volatility of a stock per day month and year.
 
 We had a meeting this morning with Kim, and Figured out that I needed to create a concrete 3rd visualisation. The portfolio idea, is going to be a bit complex. Due to a database that is going to be neccesary but also because of the amount of API calls you need to make. Therefor I had to come up with another idea. That is going to be an overvieuw of the Headquarters location of the s&p 500 companies in a map. 
+
+# day 6
+I can get my data in a list now
+
+# day 7
+The user can enter a ticker symbol, and then a graph pops up with the historical stock prices of the entered stock. When a new stock is entered a new graph comes underneath.
+
+The goal is to update the graph instead of creating a new one underneath. Also I'm trying to make a tooltip so you can hover over the line chart and click on a day so you can see the daily volatility.
+
--- Script/getinfo.js
+++ Script/getinfo.js
@@ -10,129 +10,167 @@ window.onload = function() {
 
 // get dataset
 stringOne = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="
-// stringTwo = "AAPL"
 stringThree = "&interval=5min&apikey=DFYSSV5T3EZ9UW6E"
 
 d3.select("#sic")
-      .on("click", function() {
-        // console.log("clicked on search button")
-        var inputSearch = d3.select("#sin").property("value");
-        getData(inputSearch)
-        // console.log(inputSearch)
-      });
+  .on("click", function() {
+  var inputSearch = d3.select("#sin").property("value");
+  getData(inputSearch)  
+  // updateData()      
+  });
 
 function getData(input){
-  console.log(input)
   requestTicker = stringOne + input + stringThree
   writeToJson(requestTicker)
-
-
-}
-
-// requestTicker = stringOne + stringTwo + stringThree
-requestTickerTest = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AAPL&interval=5min&apikey=DFYSSV5T3EZ9UW6E"
+  }
 
 function writeToJson(request){
-  var requests = [d3.json(request), d3.json(requestTickerTest)];
+  var requests = [d3.json(request)];
 
   // hold data
   Promise.all(requests).then(function(response) {
 
   mydata = response;
-  // mydatajoe = response[0];
 
-  // console.log(mydatajoe)
-
-  // transform into better format
-  console.log(mydata)
-
-//   d3.select("search_icon").onclick = function() {
-//   var inputSearch = d3.select("search_input").value;
-//   console.log(inputSearch)
-// }
 
   mydata.forEach(function(element){
-    // console.log(element)
-    // console.log(element["Time Series (Daily)"]["2018-03-02"])
-    // save this in variable
-    // do another for loop 
-    // then you can get all the values
-    // save them in a list
-    console.log("yo")
-
-    console.log(element["Time Series (Daily)"]["2018-11-09"]["4. close"])
-
-    var hallo = []
-
-    hallo.push([element["Time Series (Daily)"]])
-
-    hallo.forEach(function(elementyo){
-      console.log("X")
-      console.log(Object.keys(elementyo))
-    })
-
-    // var stockInfo = []
-
-    // stockInfo.push(element["Time Series (Daily"])
-
-    // stockInfo.forEach(function(elementhoi){
-    //   console.log(elementhoi)
-    // })
-
-
-    // console.log(stockInfo)
-
-    // stockInfo.forEach(function (argument) {
-    //   console.log(argument)
-    //   // body...
-    // })
-
+    var dailies = element["Time Series (Daily)"]
+    var hunderdDays = [];
+    var dates = [];
     
-   });
+    $.each(dailies, function(index, value) {
+    var closingPrice = value["4. close"]
+    hunderdDays.push({"Date": index, "Close": Number(closingPrice)});
+    dates.push(index)
+    }); 
 
+    createLineChart(hunderdDays, dates)
+    });
+
+function createLineChart(hunderdDays, dates){
+
+  // set the dimensions and margins of the graph
+  var margin = {top: 50, right: 20, bottom: 100, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+            
+  // append the svg object to the body of the page
+  // append a 'group' element to 'svg'
+  // moves the 'group' element to the top left margin
+  var svg = d3.select("#svg").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", 
+          "translate(" + margin.left + "," + margin.top + ")");
+
+  // create yScale
+  var yScale = d3.scaleLinear()
+    .domain([0, d3.max(hunderdDays, function(d){return d.Close})])
+    .range([height, 0]); 
+
+  // create xScaleDate
+  var xScaleDate = d3.scaleBand()
+    .domain(dates)
+    .range([0, width]);
+
+  // create xScale
+  var xScale = d3.scaleLinear()
+    .domain([0,100])
+    .range([0, width]);
+
+  // create yAxis
+  var yAxis = d3.axisLeft(yScale)
+
+  // append yAxis on the left side
+  svg.append("g")
+    .call(d3.axisLeft(yScale));
+            
+  // add the xAxis
+  svg.append("g")
+    .attr("transform", "translate(-10," + height + ")")
+    .call(d3.axisBottom(xScaleDate))
+    .selectAll("text")
+    .style("font", "8px times") 
+    .style("text-anchor", "end")
+    .attr("dx", "-.8em")
+    .attr("dy", ".15em")
+    .attr("transform", "rotate(-90)");
+
+  // create line
+  line = d3.line()
+    .x(function (d,i) { return xScale(i)})
+    .y(function (d) { return yScale(d.Close)})
+
+  // append path
+  svg.append("path")
+    .datum(hunderdDays)
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-width", 1.5)
+    .attr("stroke-linejoin", "round")
+    .attr("stroke-linecap", "round")
+    .attr("d", line);
+  }
+
+  // Define the div for the tooltip
+  var div = d3.select("body").append("div") 
+      .attr("class", "tooltip")       
+      .style("opacity", 0);
+
+
+      // Add the scatterplot
+    svg.selectAll("dot")  
+        .data(hunderdDays)     
+    .enter().append("circle")               
+        .attr("r", 5)   
+        .attr("cx", function(d) { return x(d.Date); })     
+        .attr("cy", function(d) { return y(d.Close); })   
+        .on("mouseover", function(d) {    
+            div.transition()    
+                .duration(200)    
+                .style("opacity", .9);    
+            div .html(formatTime(d.Date) + "<br/>"  + d.Close)  
+                .style("left", (d3.event.pageX) + "px")   
+                .style("top", (d3.event.pageY - 28) + "px");  
+            })          
+        .on("mouseout", function(d) {   
+            div.transition()    
+                .duration(500)    
+                .style("opacity", 0); 
+        });
 
 }).catch(function(e){
     throw(e);
 });
-
 };
-}
+};
 
-// stringtwo has to contain the request from the user. that Needs to be checked (if ticker symbol exists)
-// var requests = [d3.json(requestTicker), d3.json(requestTickerTest)];
+// function updateData(){
 
-// // hold data
-// Promise.all(requests).then(function(response) {
-//   mydata = response;
-//   mydatajoe = response[0];
-//   console.log(mydatajoe)
+//   // Scale the range of the data again 
+//   line = d3.line()
+//     .x(function (d,i) { return xScale(i)})
+//     .y(function (d) { return yScale(d.Close)})
 
-//   // transform into better format
-//   console.log(mydata)
+//   // Select the section we want to apply our changes to
+//   var svg = d3.select("#svg").transition();
 
-//   d3.select("search_icon").onclick = function() {
-//   var inputSearch = d3.select("search_input").value;
-//   console.log(inputSearch)
-// }
-
-//   mydata.forEach(function(element){
-//     console.log(element)
-//     // console.log(element["Time Series (Daily)"]["2018-03-02"])
-//     // save this in variable
-//     // do another for loop 
-//     // then you can get all the values
-//     // save them in a list
-//     console.log(element["Time Series (5min)"])
-
-    
-//    });
-
-
-// }).catch(function(e){
-//     throw(e);
-// });
+//   // Make the changes
+//     svg.select(".line")   // change the line
+//         .duration(750)
+//         .attr("d", valueline(hunderdDays));
+//     svg.select(".x.axis") // change the x axis
+//         .duration(750)
+//         .call(xAxis);
+//     svg.select(".y.axis") // change the y axis
+//         .duration(750)
+//         .call(yAxis);
+//       }
 
 // };
+// }
+
 
 
 
